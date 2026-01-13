817239852b14bd53de0c762d4a615f8403486e4e
Chinua Eric Robinson <98785033+Crobinson144@users.noreply.github.com>
2026-01-13 04:35:24 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -17,3 +17,8 @@ activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFa
 X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+# Reads in the test data files
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
