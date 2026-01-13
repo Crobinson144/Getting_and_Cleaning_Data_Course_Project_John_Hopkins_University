fe6c356c8786e773aaaaf1ad9f1125a055e7ae56
Chinua Eric Robinson <98785033+Crobinson144@users.noreply.github.com>
2026-01-13 04:33:23 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -7,3 +7,9 @@
 # into a tidy data set for analysis.
 ############################################
 
+library(dplyr)
+
+# Reads in the feature names and activity labels
+features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE)
+
