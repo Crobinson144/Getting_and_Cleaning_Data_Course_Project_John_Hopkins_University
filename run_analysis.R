26a8f3071a86359fbcce36ea2ab91fdbbab815fa
Chinua Eric Robinson <98785033+Crobinson144@users.noreply.github.com>
2026-01-13 04:36:57 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -22,3 +22,20 @@ subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+# Combines the training and test data together
+X <- rbind(X_train, X_test)
+y <- rbind(y_train, y_test)
+subjects <- rbind(subject_train, subject_test)
+
+# Adds column names so the data is easier to understand
+colnames(X) <- features$V2
+colnames(y) <- "Activity"
+colnames(subjects) <- "Subject"
+
+# Puts everything into one data frame
+data <- cbind(subjects, y, X)
+
+# Keeps only the measurements related to mean and standard deviation
+mean_std_cols <- grep("mean\\(\\)|std\\(\\)", colnames(data))
+data <- data[, c(1, 2, mean_std_cols)]
