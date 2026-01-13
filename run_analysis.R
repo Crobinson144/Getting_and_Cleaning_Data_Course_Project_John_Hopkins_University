6e611d025a687a5e90c398d96730cfb90457ea6b
Chinua Eric Robinson <98785033+Crobinson144@users.noreply.github.com>
2026-01-13 04:31:39 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,9 @@
+############################################
+# Getting and Cleaning Data Course Project
+# Johns Hopkins University - Coursera
+#
+# This script cleans and prepares the
+# UCI Human Activity Recognition dataset
+# into a tidy data set for analysis.
+############################################
 
